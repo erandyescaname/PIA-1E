5ebbcf1f799ee866264d25de919935cc3a5e4819
erandyescaname <erandy.escanameb@uanl.edu.mx>
2024-09-11 23:10:43 -0600

    Add files via upload

---
+++ T5EYEB.ps1
@@ -0,0 +1,6 @@
+﻿Get-Content -Path C:\Users\PC\Desktop\servicios.txt
+Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service
+Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service | Start-Service
+Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service
+Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service | Stop-Service
+Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service
