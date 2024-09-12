f5aa08f6b53c57c7be4bac58adb5d94c56fe979a
erandyescaname <erandy.escanameb@uanl.edu.mx>
2024-09-11 23:14:07 -0600

    Delete T5EYEB.ps1

--- T5EYEB.ps1
+++
@@ -1,6 +0,0 @@
-﻿Get-Content -Path C:\Users\PC\Desktop\servicios.txt
-Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service
-Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service | Start-Service
-Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service
-Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service | Stop-Service
-Get-Content -Path C:\Users\PC\Desktop\servicios.txt | Get-Service
